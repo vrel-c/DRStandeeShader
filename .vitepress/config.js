9c51890314f231bd02a70d6823400922dd874cb6
vrel-c <sailara.pokemongo@gmail.com>
2026-06-30 22:15:36 +0900

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -2,6 +2,9 @@ export default {
   title: "DRStandeeShader / タテカンシェーダ",
   base: '/DRStandeeShader/',
   description: "『DRStandeeShader / タテカンシェーダ』のオンラインマニュアルです。",
+  head: [
+    ['meta', { name: 'robots', content: 'noindex, nofollow' }]
+  ],
   themeConfig: {
     // 左側のメニュー（サイドバー）の設定
     siteTitle: "DRStandeeShader", 
