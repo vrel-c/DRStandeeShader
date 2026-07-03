b1fa4ee9ac6d2d6ddc023b89b67f73c14aa56a6b
vrel-c <sailara.pokemongo@gmail.com>
2026-07-04 03:47:37 +0900

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,25 +1,37 @@
-export default {
+import { defineConfig } from 'vitepress'
+
+export default defineConfig {
   title: "DRStandeeShader / タテカンシェーダ",
   base: '/DRStandeeShader/',
   description: "『DRStandeeShader / タテカンシェーダ』のオンラインマニュアルです。",
   head: [
     ['meta', { name: 'robots', content: 'noindex, nofollow' }]
   ],
-  themeConfig: {
-    siteTitle: "DRStandeeShader", ,
-    nav: [
-      { text: 'ver1.0.2', activeMatch: '' }
-    ],
-    sidebar: [
-      { text: '概要（トップ）', link: '/' },
-      { text: '導入方法', link: '/import' },
-      { text: 'Lite版について', link: '/lite' },
-      { text: '通常版について', link: '/standard' },
-      { text: '権利表記等', link: '/license' },
-      {
-        text: 'ver1.0.2',
-        items: []
+  srcDir: 'docs',
+  vite: {
+    publicDir: '../public' 
+  },
+  locales: {
+    root: {
+      label: '日本語',
+      lang: 'ja',
+      themeConfig: {
+        siteTitle: "DRStandeeShader",
+        nav: [
+          { text: 'ver1.0.2', activeMatch: '' }
+        ],
+        sidebar: [
+          { text: '概要（トップ）', link: '/' },
+          { text: '導入方法', link: '/import' },
+          { text: 'Lite版について', link: '/lite' },
+          { text: '通常版について', link: '/standard' },
+          { text: '権利表記等', link: '/license' },
+          {
+            text: 'ver1.0.2',
+            items: []
+          }
+        ]
       }
-    ]
+    }
   }
 }
