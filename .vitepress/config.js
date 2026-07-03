d2d60608c679e28a5475fed4c4525a8301fa9ce1
vrel-c <sailara.pokemongo@gmail.com>
2026-07-04 03:57:08 +0900

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -11,9 +11,6 @@ export default {
       lang: 'ja',
       themeConfig: {
         siteTitle: "DRStandeeShader",
-        nav: [
-          { text: 'ver1.0.2', activeMatch: '' }
-        ],
         sidebar: [
           { text: '概要（トップ）', link: '/' },
           { text: '導入方法', link: '/import' },
